48e0d59db337e3a6a7d987c29f94f551fc5f5539
Marwa Mahmoud <marwa.sw.eng@outlook.com>
2026-03-09 05:54:21 +0200

    Refactor code structure for improved readability and maintainability

---
+++ mathPass/mathPass/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿
+namespace mathPass
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.key_txt = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.link_txt = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Run_btn = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.password_txt = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // key_txt
+            // 
+            this.key_txt.Font = new System.Drawing.Font("Verdana", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.key_txt.Location = new System.Drawing.Point(212, 79);
+            this.key_txt.Name = "key_txt";
+            this.key_txt.Size = new System.Drawing.Size(602, 29);
+            this.key_txt.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 10.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.label1.Location = new System.Drawing.Point(93, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Key :";
+            // 
+            // link_txt
+            // 
+            this.link_txt.Font = new System.Drawing.Font("Verdana", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.link_txt.Location = new System.Drawing.Point(212, 172);
+            this.link_txt.Name = "link_txt";
+            this.link_txt.Size = new System.Drawing.Size(602, 29);
+            this.link_txt.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Verdana", 10.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.label2.Location = new System.Drawing.Point(87, 174);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 23);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Link :";
+            // 
+            // Run_btn
+            // 
+            this.Run_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Run_btn.Font = new System.Drawing.Font("Verdana", 10.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Run_btn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.Run_btn.Location = new System.Drawing.Point(391, 277);
+            this.Run_btn.Name = "Run_btn";
+            this.Run_btn.Size = new System.Drawing.Size(173, 45);
+            this.Run_btn.TabIndex = 4;
+            this.Run_btn.Text = "Run";
+            this.Run_btn.UseVisualStyleBackColor = true;
+            this.Run_btn.Click += new System.EventHandler(this.Run_btn_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Verdana", 10.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.label3.Location = new System.Drawing.Point(52, 448);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(135, 23);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Password : ";
+            // 
+            // password_txt
+            // 
+            this.password_txt.Font = new System.Drawing.Font("Verdana", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.password_txt.Location = new System.Drawing.Point(218, 385);
+            this.password_txt.Multiline = true;
+            this.password_txt.Name = "password_txt";
+            this.password_txt.ReadOnly = true;
+            this.password_txt.Size = new System.Drawing.Size(602, 171);
+            this.password_txt.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(945, 594);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.password_txt);
+            this.Controls.Add(this.Run_btn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.link_txt);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.key_txt);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Math Password";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox key_txt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox link_txt;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button Run_btn;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox password_txt;
+    }
+}
+
---
+++ mathPass/mathPass/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mathPass
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Run_btn_Click(object sender, EventArgs e)
+        {
+            string key = key_txt.Text;
+            string link = link_txt.Text;
+            link_txt.Text = link.ToUpper();
+            if(key_txt.Text.Length < 4)
+                MessageBox.Show("Key Length must be Larger than 4 Character", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            
+            else if(link_txt.Text.Length < 5)
+                MessageBox.Show("Link Length must be Larger than 5 Character", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+            else if (link_txt.Text.Contains("www."))
+                MessageBox.Show("Link must contains .www", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            else
+            {
+                long[] New_Key = ToNumber(key);
+                long[] New_Link = ToNumber(link);
+                long[] Formated_Key = FormateKey(New_Key);
+                long[] Formated_Link = FormateLink(New_Link);
+                long checkSum_key = CheckSum(Formated_Key);
+                long checkSum_Link = CheckSum(Formated_Link);
+                long checkSum = 0;
+                string sum = "";
+                sum += checkSum_key.ToString() + checkSum_Link.ToString();
+                for (int i = 0; i < sum.Length; i++)
+                {
+                    checkSum += (int)sum[i] - 48;
+                }
+                long[] keyPlusLink = new long[18];
+                long[] password = new long[18];
+                string result = "";
+                for (int i = 0, j = 0; i < 18; i++, j++)
+                {
+                    if (j == 6)
+                        j = 0;
+                    keyPlusLink[i] = (Formated_Link[i] + Formated_Key[j]) * checkSum;
+                }
+                for (int i = 0; i < 18; i++)
+                {
+                    keyPlusLink[i] = keyPlusLink[i] / 10;
+                }
+                for (int i = 0; i < 18; i++)
+                {
+                    password[i] = keyPlusLink[i] % 95 + 32;
+                }
+
+                for (int i = 0; i < password.Length; i++) 
+                {
+                    
+                    result += (char)password[i];
+                }
+                password_txt.Text = result; 
+            }
+        }
+        private long[] ToNumber(string str)
+        {
+            List<long> res = new List<long>();
+            for (int i = 0; i < str.Length; i++) 
+            {
+                res.Add(str[i]);
+            }
+            return res.ToArray();
+        }
+
+        private long[] FormateKey(long[] arr)
+        {
+            List<long> res = new List<long>();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                res.Add(arr[i]);
+            }
+            if (arr.Length == 4)
+            {
+                res.Add(arr[0] * arr[1]);
+                res.Add(arr[2] * arr[3]);
+            }
+            else if (arr.Length == 5)
+            {
+                res.Add(arr[0] * arr[1]);
+            }
+            else if (arr.Length > 6 && arr.Length < 24)
+            {
+                long[] arr2 = new long[6];
+                for (int i = 0; i < 6; i++)
+                {
+                    int h = i + 6;
+                    while (h < arr.Length)
+                    {
+                        arr[i] *= arr[h];
+                        h += 6;
+                    }
+                }
+
+                for (int i = 0; i < 6; i++)
+                {
+                    arr2[i] = arr[i];
+                }
+                return arr2;
+            }
+            else if (arr.Length >= 24) 
+            {
+                long[] arr2 = new long[6];
+                for (int i = 0; i < 6; i++)
+                {
+                    int h = i + 6;
+                    while (h < arr.Length)
+                    {
+                        if (h < 18)
+                            arr[i] *= arr[h];
+                        else
+                            arr[i] += arr[h];
+                        h += 6;
+                    }
+                }
+
+                for (int i = 0; i < 6; i++)
+                {
+                    arr2[i] = arr[i];
+                }
+                return arr2; 
+            }
+
+            return res.ToArray();
+        }
+        private long[] FormateLink(long[] arr)
+        {
+            long[] arr2 = new long[18];
+            List<long> res = new List<long>();
+            if (arr.Length < 18) 
+            {
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    res.Add(arr[i]);
+                }
+                for (int i = 0; i < 18 - arr.Length; i++) 
+                {
+                    if (i <= 10)
+                    {
+                        res.Add(res[i] * res[i + 1]);
+                    }
+                    else
+                    {
+                        res.Add(res[i] + res[i + 1]);
+                    }
+                }
+                return res.ToArray();
+            }
+            else if (arr.Length > 18)
+            {
+                for (int i = 0; i < arr.Length - 18; i++)
+                {
+                    arr[i] = arr[i] + arr[arr.Length - 1 - i];
+                    arr[arr.Length - 1 - i] = 48;
+                }
+            }
+            for (int i = 0; i < 18; i++)
+            {
+                arr2[i] = arr[i];
+            }
+            return arr2;
+        }
+        private long CheckSum(long[] arr)
+        {
+            long result = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                string s = arr[i].ToString();
+                for (int j = 0; j < s.Length; j++)
+                {
+                    result += (int)s[j] - 48;
+                }
+            }
+            
+            return result;
+        }
+
+    }
+}
